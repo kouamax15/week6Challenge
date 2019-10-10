29f417e5e283846c06372b5894a85e50361dcaa4
kouamax15 <kouamax15@gmail.com>
2019-10-10 15:44:27 -0400

    first commit thursday

---
+++ src/main/java/com/example/demo/Department.java
@@ -0,0 +1,46 @@
+package com.example.demo;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Department {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long departmentID;
+
+    private String departmentName;
+
+
+
+    @OneToMany(mappedBy = "department", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    public Set<Employee> employees;
+
+    public long getDepartmentID() {
+        return departmentID;
+    }
+
+    public void setDepartmentID(long departmentID) {
+        this.departmentID = departmentID;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public Department() {
+    }
+}
